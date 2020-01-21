504a65eb809ee45605e115280adf4f0928145d5f
Igor <GI_Something@protonmail.com>
2020-01-21 23:39:30 +0200

    feat: Add possibility to read training data from directory

--- src/recognition/TrainingData.java
+++ src/recognition/TrainingData.java
@@ -1,5 +1,9 @@
 package recognition;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TrainingData {
     public static final double[][] trainingInputNumbersGrid5x3 = {
             {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1},  //0
@@ -24,4 +28,21 @@ public class TrainingData {
             {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
+
+    public static double[][] fromDirectory(String directoryPath, int layerSize) throws FileNotFoundException {
+        Scanner scn;
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        double[][] trainingData = new double[files.length][layerSize];
+
+        for (int i = 0; i < files.length; i++) {
+            scn = new Scanner(files[i]);
+            int index = 0;
+            while (index < layerSize) {
+                trainingData[i][index++] = scn.nextDouble();
+            }
+            scn.close();
+        }
+        return trainingData;
+    }
 }
